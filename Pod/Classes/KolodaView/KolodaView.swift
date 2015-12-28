d6990c96bfc56e168f52f669349f820be6e98a7d
Eugene Andreyev <justandjack@hotmail.com>
2015-12-28 22:03:12 +0200

    Changed animation canceling order

--- Pod/Classes/KolodaView/KolodaView.swift
+++ Pod/Classes/KolodaView/KolodaView.swift
@@ -387,6 +387,8 @@ public class KolodaView: UIView, DraggableCardDelegate {
         if !visibleCards.isEmpty {
             
             for (index, currentCard) in visibleCards.enumerate() {
+                currentCard.removeAnimations()
+                
                 var frameAnimation: POPPropertyAnimation
                 if let delegateAnimation = delegate?.koloda(kolodaBackgroundCardAnimation: self) where delegateAnimation.property.name == kPOPViewFrame {
                     frameAnimation = delegateAnimation
@@ -423,7 +425,6 @@ public class KolodaView: UIView, DraggableCardDelegate {
                 
                 currentCard.userInteractionEnabled = index == 0
                 frameAnimation.toValue = NSValue(CGRect: cardFrame)
-                currentCard.removeAnimations()
                 currentCard.pop_addAnimation(frameAnimation, forKey: "frameAnimation")
             }
         } else {
