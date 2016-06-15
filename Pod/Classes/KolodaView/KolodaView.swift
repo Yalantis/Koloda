6ba0e676cbf5d6c32946d8256a5da8c4620b9f93
13668 <ferenc.beutel@nordakademie.de>
2016-06-15 09:42:03 +0200

    Add parameters to subscribeForNotifications

--- Pod/Classes/KolodaView/KolodaView.swift
+++ Pod/Classes/KolodaView/KolodaView.swift
@@ -120,16 +120,16 @@ public class KolodaView: UIView, DraggableCardDelegate {
     //MARK: Lifecycle
     
     deinit {
-        unsubsribeFromNotifications()
+        unsubscribeFromNotifications()
     }
     
     //MARK: Configurations
     
-    public func subscribeForNotifications(selector: Selector) {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: selector, name: UIDeviceOrientationDidChangeNotification, object: nil)
+    public func subscribeForNotifications(notificationCenter: NSNotificationCenter, selector: Selector, notification: NSObject) {
+        notificationCenter.addObserver(self, selector: selector, name: notification, object: nil)
     }
     
-    public func unsubsribeFromNotifications() {
+    public func unsubscribeFromNotifications() {
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
