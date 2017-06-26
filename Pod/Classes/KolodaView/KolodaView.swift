e0318deddb0f50397f7cbc211b8d0df4488ab92e
Noor Dawod <noor@fineswap.com>
2017-06-26 15:30:18 +0200

    Used a local variable.

--- Pod/Classes/KolodaView/KolodaView.swift
+++ Pod/Classes/KolodaView/KolodaView.swift
@@ -177,16 +177,16 @@ open class KolodaView: UIView, DraggableCardDelegate {
     
     // MARK: Frames
     open func frameForCard(at index: Int) -> CGRect {
+        let backgroundCardsTopMargin = defaultBackgroundCardsTopMargin
         let bottomOffset: CGFloat = 0
         let topOffset = backgroundCardsTopMargin * CGFloat(countOfVisibleCards - 1)
-        let backgroundCardsScalePercent = defaultBackgroundCardsScalePercent
-        let scalePercent = backgroundCardsScalePercent
+        let scalePercent = defaultDackgroundCardsScalePercent
         let width = self.frame.width * pow(scalePercent, CGFloat(index))
         let xOffset = (self.frame.width - width) / 2
         let height = (self.frame.height - bottomOffset - topOffset) * pow(scalePercent, CGFloat(index))
         let multiplier: CGFloat = index > 0 ? 1.0 : 0.0
         let prevCardFrame = index > 0 ? frameForCard(at: max(index - 1, 0)) : .zero
-        let yOffset = (prevCardFrame.height - height + prevCardFrame.origin.y + backgroundCardsScalePercent) * multiplier
+        let yOffset = (prevCardFrame.height - height + prevCardFrame.origin.y + ackgroundCardsTopMargin) * multiplier
         let frame = CGRect(x: xOffset, y: yOffset, width: width, height: height)
         
         return frame
