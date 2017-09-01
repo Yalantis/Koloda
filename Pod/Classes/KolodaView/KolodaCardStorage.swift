80bf28a638f4c3acf710fb5723f4cd463ccbd830
Noor Dawod <noor@fineswap.com>
2017-09-01 20:15:48 +0200

    Previous solution was awkward, fixed so that values are set in KolodaView and not in DraggableCardView.

--- Pod/Classes/KolodaView/KolodaCardStorage.swift
+++ Pod/Classes/KolodaView/KolodaCardStorage.swift
@@ -27,7 +27,18 @@ extension KolodaView {
     
     func configureCard(_ card: DraggableCardView, at index: Int) {
         let contentView = dataSource!.koloda(self, viewForCardAt: index)
-        card.configure(contentView, overlayView: dataSource?.koloda(self, viewForCardOverlayAt: index), in: self)
+        card.configure(contentView, overlayView: dataSource?.koloda(self, viewForCardOverlayAt: index))
+
+        //Reconfigure drag animation constants from Koloda instance.
+        if let rotationMax = self.rotationMax {
+            card.rotationMax = rotationMax
+        }
+        if let rotationAngle = self.rotationAngle {
+            card.rotationAngle = rotationAngle
+        }
+        if let scaleMin = self.scaleMin {
+            card.scaleMin = scaleMin
+        }
     }
     
 }
