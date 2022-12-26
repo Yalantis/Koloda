2c2ab549dbb2e340d32e6cb7d244cb229403b530
Wei <wei.cao@pinkoi.com>
2022-12-26 13:26:05 +0800

    [Update] pop to 1.0.12

--- Package.swift
+++ Package.swift
@@ -12,13 +12,19 @@ let package = Package(
         .library(name: "Koloda", targets: ["Koloda"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vmzhivetyev/pop-spm-package.git", from: "1.0.4"),
     ],
     targets: [
         .target(
             name: "Koloda",
-            dependencies: [.product(name: "pop", package: "pop-spm-package")],
+            dependencies: [
+                "pop"
+            ],
             path: "Pod/Classes/"
-        )
+        ),
+        .binaryTarget(
+            name: "pop",
+            url: "https://github.com/Wei18/pop/releases/download/1.0.12/pop.zip",
+            checksum: "a002c9b0e6d29cd83b54af4dbcdb8012a229b381f4ebaa13d43b9719dd5af54e"
+        ),
     ]
 )
